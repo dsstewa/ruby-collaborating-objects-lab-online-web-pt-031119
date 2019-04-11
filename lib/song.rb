478c4457b1bb1228b4e059cd473396766e9fd5a3
David Stewart <davidstewart@gmail.com>
2019-04-11 04:00:14 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -15,7 +15,7 @@ attr_accessor :song, :artist, :name
   def self.new_by_filename(filename)
     array = filename.split(" - ")
     new_song = Song.new(array[1])
-    artist = Artist.new(array[0])
+    artist = Artist.find_or_create_by_name(array[0])
     new_song.artist = artist
     #binding.pry
     new_song
