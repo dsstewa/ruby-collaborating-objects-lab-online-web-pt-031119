0220274a986b8e859a1d9885ecfdc03a8c0184e5
David Stewart <davidstewart@gmail.com>
2019-04-11 03:25:16 +0000

    Automatically backed up by Learn

---
+++ lib/song.rb
@@ -0,0 +1,24 @@
+require 'pry'
+
+class Song
+
+attr_accessor :song, :artist, :name
+
+  
+  def initialize(song)
+    @song = song
+    @name = song
+  end
+  
+
+  
+  def self.new_by_filename(filename)
+    
+  
+  
+  end
+
+  
+  
+end
+
