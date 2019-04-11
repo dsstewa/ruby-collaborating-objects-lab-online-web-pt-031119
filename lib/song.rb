41787bbbb542b1fae4127cc0b1113e4f5b1842f8
David Stewart <davidstewart@gmail.com>
2019-04-11 04:06:48 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -16,6 +16,7 @@ attr_accessor :song, :artist, :name
     array = filename.split(" - ")
     new_song = Song.new(array[1])
     artist = Artist.new(array[0])
+    artist.add_song(array[1])
     new_song.artist = artist
     #binding.pry
     new_song
