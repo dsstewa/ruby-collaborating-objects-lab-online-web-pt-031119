523c0be535f492abd8d2d2e7c524c7a61e1808b3
David Stewart <davidstewart@gmail.com>
2019-04-11 03:35:38 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -14,9 +14,9 @@ attr_accessor :song, :artist, :name
   
   def self.new_by_filename(filename)
     array = filename.split(" - ")
-    Song.new(array[1])
-    Song.artist = array[0]
-  end
+    new_song = Song.new(array[1])
+    new_song.artist = array[0]
+    new_song
 
   
   
